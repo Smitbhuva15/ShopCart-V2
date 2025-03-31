a77ad612ecbbc2b452e7f4d33772fa715e4f232e
smitbhuva15 <itssmit123@gmail.com>
2025-03-31 18:18:22 +0530

    Search product on home page

--- src/compo/Home/Banner.tsx
+++ src/compo/Home/Banner.tsx
@@ -1,9 +1,11 @@
 "use client"
 import React, { useState } from 'react'
 import SelectedCategory from '../Common/SelectedCategory';
+import { product_data } from '@/lib/product';
+import Link from 'next/link';
 
 const title = (
-    
+
     <h2>Find Your Perfect Match from <span>Thousands</span> of Products</h2>
 )
 const desc = "We have the largest collection of product";
@@ -11,13 +13,17 @@ const desc = "We have the largest collection of product";
 export default function Banner() {
 
     const [searchInput, setSearchInput] = useState("")
+    const [filterProducts, setFilterProducts] = useState(product_data);
 
 
     const handelSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchInput(e.target.value)
-        console.log(searchInput)
+    const filter_Products = product_data.filter((product) => product.name.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase()))
+       setFilterProducts(filter_Products)
+
     }
 
+  
 
     return (
         <div className='banner-section style-4'>
@@ -35,6 +41,14 @@ export default function Banner() {
 
                     <p>
                         {desc}
+                        <ul className='lab-ul'>
+                            {
+                                searchInput && filterProducts.map((product, i) => (
+                                    <li ><Link href={`/shop/${product.id}`}>{product.name}</Link> </li>
+                                )
+                                )
+                            }
+                        </ul>
                     </p>
                 </div>
             </div>
